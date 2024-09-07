ada4d37ac4e7c2a2114c60467e7d7dc0b98131b3
Rey <47404421+moonjs0113@users.noreply.github.com>
2024-09-07 16:32:01 +0900

    Backjoon StepByStep Level16 From 5 to 7

--- Backjoon/StepByStep/Level16_StackQueueDeque.swift
+++ Backjoon/StepByStep/Level16_StackQueueDeque.swift
@@ -82,3 +82,103 @@ Loop: while true {
     }
     print(stack.isEmpty ? "yes" : "no")
 }
+
+/*
+ 도키도키 간식드리미
+ https://www.acmicpc.net/problem/12789
+ */
+readLine()
+var i = 1
+var stack: [Int] = []
+for num in readLine()!.split(separator: " ") {
+    let n = Int(num)!
+    if n == i {
+        i += 1
+        while (stack.last ?? 0) == i {
+            i += 1
+            stack.popLast()
+        }
+        continue
+    }
+    if (stack.last ?? 1001) < n {
+        i = -1
+        print("Sad")
+        break
+    }
+    stack.append(n)
+}
+if i != -1 { print("Nice") }
+
+/*
+ 큐 2
+ https://www.acmicpc.net/problem/18258
+ */
+
+// General Code
+var queue: [Int?] = Array(repeating: nil, count: 2_000_000)
+var headPointer = 0
+var inputPointer = 0
+(0..<Int(readLine()!)!).forEach { _ in
+    let command = readLine()!
+    if command.contains(" ") {
+        queue[inputPointer] = Int(command.split(separator: " ").last!)!
+        inputPointer += 1
+    } else {
+        let isEmpty = (headPointer == inputPointer)
+        switch command {
+        case "pop":
+            if !isEmpty {
+                print(queue[headPointer]!)
+                queue[headPointer] = nil
+                headPointer += 1
+            } else {
+                print(-1)
+            }
+        case "size": print(inputPointer - headPointer)
+        case "empty": print(isEmpty ? 1 : 0)
+        case "front": print(isEmpty ? -1 : queue[headPointer]!)
+        default: print(inputPointer == 0 ? -1 : (queue[inputPointer-1] ?? -1)) // back
+        }
+    }
+}
+
+// Using File IO -> 백준 입출력 시간 제한
+var queue: [Int] = []
+var pointer = 0
+var result = ""
+let fileIO = FileIO()
+(0..<fileIO.readInt()).forEach { _ in
+    let command = fileIO.readStringToIntValue()
+    let isEmpty = !(pointer < queue.count)
+    switch command {
+    case 448: // push
+        queue.append(fileIO.readInt())
+    case 335: // pop
+        if !isEmpty {
+            result += "\(queue[pointer])\n"
+            pointer += 1
+        } else {
+            result += "-1\n"
+        }
+    case 443: result += "\(queue.count - pointer)\n" // size
+    case 559: result += "\(isEmpty ? 1 : 0)\n" // empty
+    case 553: result += "\(isEmpty ? -1 : queue[pointer])\n" // front
+    case 401: result += "\(isEmpty ? -1 : queue.last!)\n" // back
+    default: return
+    }
+}
+print(result)
+
+/*
+ 카드2
+ https://www.acmicpc.net/problem/2164
+ */
+let num = Int(readLine()!)!
+if num < 3 { print(num) }
+else {
+    var i = 4
+    while num > i {
+        i *= 2
+    }
+    print((num - i / 2) * 2)
+}
