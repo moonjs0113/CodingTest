9204bcd4a7407c58c85afc52cd9682b6a17f880f
Rey <47404421+moonjs0113@users.noreply.github.com>
2024-09-10 11:59:56 +0900

    Backjoon StepByStep Level17 Complete

--- Backjoon/StepByStep/Level_17_Combinatorics.swift
+++ Backjoon/StepByStep/Level_17_Combinatorics.swift
@@ -26,3 +26,32 @@ print(Int(pow(Float(2), Float(num))))
  */
 let num = Int(readLine()!)!
 print(num == 0 ? 1 : (1...num).reduce(1, *))
+
+/*
+ 이항 계수 1
+ https://www.acmicpc.net/problem/11050
+ */
+var nums = readLine()!.split(separator: " ").map { Int($0)! }
+var result = 1
+(0..<nums[1]).forEach { _ in
+    result *= nums[0]
+    nums[0] -= 1
+}
+result /= (nums[1] == 0 ? 1 : (1...nums[1]).reduce(1, *))
+print(result)
+
+/*
+ 다리 놓기
+ https://www.acmicpc.net/problem/1010
+ */
+(0..<Int(readLine()!)!).forEach { _ in
+    var nums = readLine()!.split(separator: " ").map { Int($0)! }
+    if nums[0] > (nums[1] / 2) { nums[0] = nums[1] - nums[0] }
+    var result = 1
+    (0..<nums[0]).forEach { i in
+        result *= nums[1]
+        result /= (i + 1)
+        nums[1] -= 1
+    }
+    print(result)
+}
