66e03c09ca899c67acfc57a3df1342b3dc535b95
Rey <47404421+moonjs0113@users.noreply.github.com>
2024-09-11 14:19:01 +0900

    LeetCode Convert 1D Array Into 2D Array

---
+++ LeetCode/Convert1DArrayInto2DArray.swift
@@ -0,0 +1,12 @@
+/*
+  Convert 1D Array Into 2D Array
+  URL: https://leetcode.com/problems/convert-1d-array-into-2d-array
+  Difficulty: Medium
+  Topic : Array, Matrix, Simulation
+*/
+
+class Solution {
+    func construct2DArray(_ original: [Int], _ m: Int, _ n: Int) -> [[Int]] {
+        (original.count != m * n) ? [] : (0..<m).map { i in Array(original[i*n..<(i+1)*n]) }
+    }
+}
