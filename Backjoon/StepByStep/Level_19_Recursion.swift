9f7b2dd19007f22b667dc2813212f3e1bd29147c
Rey <47404421+moonjs0113@users.noreply.github.com>
2024-09-14 14:14:22 +0900

    Backjoon StepByStep Level 19 재귀의 귀재

--- Backjoon/StepByStep/Level_19_Recursion.swift
+++ Backjoon/StepByStep/Level_19_Recursion.swift
@@ -26,3 +26,23 @@ func fibonacci(n: Int) -> Int {
     return fibonacci(n: n - 1) + fibonacci(n: n - 2)
 }
 print(fibonacci(n: num))
+
+/*
+ 재귀의 귀재
+ https://www.acmicpc.net/problem/25501
+ */
+var count = 0
+func recursion(_ s: [Character], _ l: Int, _ r: Int) -> Int {
+    count += 1
+    if l >= r { return 1 }
+    else if s[l] != s[r] { return 0 }
+    else { return recursion(s, l+1, r-1) }
+}
+
+func isPalindrome(_ s: [Character]) -> Int {
+    return recursion(s, 0, s.count - 1)
+}
+(0..<Int(readLine()!)!).forEach { _ in
+    count = 0
+    print("\(isPalindrome(Array(readLine()!))) \(count)")
+}
