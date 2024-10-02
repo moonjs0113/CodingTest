f874efe6f3f6ee5f8d50096d206e0571aacbeb55
Rey <47404421+moonjs0113@users.noreply.github.com>
2024-10-02 11:42:15 +0900

    LeetCode Rank Transform of an Array

---
+++ LeetCode/Rank Transform of an Array.swift
@@ -0,0 +1,17 @@
+/*
+  Rank Transform of an Array
+  URL: https://leetcode.com/problems/rank-transform-of-an-array
+  Difficulty: Medium
+  Topic : Array, Hash Table, Sorting
+*/
+
+class Solution {
+    func arrayRankTransform(_ arr: [Int]) -> [Int] {
+        var dict: [Int:Int] = [:]
+        arr.forEach { dict[$0] = 0 }
+        dict.sorted { $0.key < $1.key }.enumerated().forEach {
+            dict[$0.element.key] = $0.offset + 1
+        }
+        return arr.map { dict[$0] ?? 0 }
+    }
+}
