59c067530eb00174defcb467042dec113cb53074
Rey <47404421+moonjs0113@users.noreply.github.com>
2024-10-15 15:35:55 +0900

    LeetCode Separate Black and White Balls

--- LeetCode/Separate Black and White Balls.swift
+++ LeetCode/Separate Black and White Balls.swift
@@ -1,9 +1,9 @@
-"""
+/*
   Separate Black and White Balls
   URL: https://leetcode.com/problems/separate-black-and-white-balls
   Difficulty: Medium
   Topic: Two Pointers, String, Greedy
-"""
+*/
 
 class Solution {
     func minimumSteps(_ s: String) -> Int {
