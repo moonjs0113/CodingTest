165317055d8ac750e76db7e3ea5a7f40488d18f7
Rey <47404421+moonjs0113@users.noreply.github.com>
2024-11-11 15:23:50 +0900

    Backjoon 13417 카드 문자열

---
+++ Backjoon/13417_카드 문자열.swift
@@ -0,0 +1,17 @@
+"""
+ 카드 문자열
+ https://www.acmicpc.net/problem/13417
+"""
+
+import Foundation
+
+(0..<Int(readLine()!)!).forEach { _ in
+    let n = Int(readLine()!)!
+    var input = readLine()!.components(separatedBy: " ")
+    var result = input.first!
+    for i in 1..<n {
+        result = input[i] <= "\(result.first!)" ? input[i] + result : result + input[i]
+    }
+    print(result)
+}
+
