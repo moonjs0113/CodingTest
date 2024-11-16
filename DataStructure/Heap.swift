06616caddedcab13c957b097bfc4b7cd94abf43c
Rey <47404421+moonjs0113@users.noreply.github.com>
2024-11-16 14:59:09 +0900

    Update Heap

--- DataStructure/Heap.swift
+++ DataStructure/Heap.swift
@@ -9,74 +9,69 @@
 import Foundation
 
 class Heap<T: Comparable> {
-    var elements: [T] = []
+    var heap: [T] = []
     let priorityFunction: (T, T) -> Bool
 
     init(priorityFunction: @escaping (T, T) -> Bool) {
         self.priorityFunction = priorityFunction
     }
 
-    var isEmpty: Bool { elements.isEmpty }
+    var isEmpty: Bool { heap.isEmpty }
 
-    var count: Int { elements.count }
+    var count: Int { heap.count }
 
-    var getRootValue: T? { elements.first }
+    var getRootValue: T? { heap.first }
 
-    func insert(_ value: T) {
-        elements.append(value)
-        siftUp(from: elements.count - 1)
+    public func push(_ value: T) {
+        heap.append(value)
+        siftDown(from: 0, to: count-1)
     }
 
-    func remove() -> T? {
+    public func pop() -> T? {
         guard !isEmpty else { return nil }
-        if count == 1 {
-            return elements.removeLast()
-        } else {
-            let value = elements[0]
-            elements[0] = elements.removeLast()
-            siftDown(from: 0)
+        let lastItem = heap.removeLast()
+        if !isEmpty {
+            let value = heap[0]
+            heap[0] = lastItem
+            siftUp(from: 0)
             return value
         }
+        return lastItem
+    }
+
+    private func siftDown(from startIndex: Int, to index: Int) {
+        let newValue = heap[index]
+        var currentIndex = index
+        while currentIndex > startIndex {
+            let parentIndex = (currentIndex - 1) >> 1
+            let parent = heap[parentIndex]
+            if priorityFunction(newValue,parent) {
+                heap[currentIndex] = parent
+                currentIndex = parentIndex
+                continue
+            }
+            break
+        }
+        heap[currentIndex] = newValue
     }
 
     private func siftUp(from index: Int) {
-        var childIndex = index
-        let childValue = elements[childIndex]
-        var parentIndex = (childIndex - 1) / 2 // parentIndex
-
-        while childIndex > 0 && priorityFunction(childValue, elements[parentIndex]) {
-            elements[childIndex] = elements[parentIndex]
-            childIndex = parentIndex
-            parentIndex = (childIndex - 1) / 2 // parentIndex
-        }
+        var currentIndex = index
+        let endIndex = self.count
+        let startIndex = index
+        let newValue = heap[index]
+        var childIndex = 2 * index + 1
         
-        elements[childIndex] = childValue
-    }
-
-    private func siftDown(from index: Int) {
-        var parentIndex = index
-        let length = count
-
-        while true {
-            let leftChildIndex = (2 * parentIndex) + 1 // leftChildIndex
-            let rightChildIndex = (2 * parentIndex) + 2 // rightChildIndex
-            var optionalParentSwapIndex: Int?
-
-            if leftChildIndex < length && priorityFunction(elements[leftChildIndex], elements[parentIndex]) {
-                optionalParentSwapIndex = leftChildIndex
+        while childIndex < endIndex {
+            let rightIndex = childIndex + 1
+            if rightIndex < endIndex && !priorityFunction(heap[childIndex],heap[rightIndex]) {
+                childIndex = rightIndex
             }
-
-            if rightChildIndex < length && priorityFunction(elements[rightChildIndex], elements[optionalParentSwapIndex ?? parentIndex]) {
-                optionalParentSwapIndex = rightChildIndex
-            }
-
-            guard let swapIndex = optionalParentSwapIndex else {
-                return
-            }
-
-            elements.swapAt(parentIndex, swapIndex)
-            parentIndex = swapIndex
+            heap[currentIndex] = heap[childIndex]
+            currentIndex = childIndex
+            childIndex = 2 * currentIndex + 1
         }
+        heap[currentIndex] = newValue
+        siftDown(from: startIndex, to: currentIndex)
     }
 }
-
