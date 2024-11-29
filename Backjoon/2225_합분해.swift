66049dedcb3ae3617343ebbae153ef9c0d2e2919
Rey <47404421+moonjs0113@users.noreply.github.com>
2024-11-29 16:16:34 +0900

    Backjoon 2225 합분해

---
+++ Backjoon/2225_합분해.swift
@@ -0,0 +1,19 @@
+/*
+  합분해
+  https://www.acmicpc.net/problem/2225
+*/
+
+let nk = readLine()!.split(separator: " ").map { Int(String($0))! }
+var dp = Array(repeating: Array(repeating: 0, count: nk[0]+1), count: nk[1]+1)
+
+for k in 1..<nk[1]+1 {
+    for n in 0..<nk[0]+1 {
+        if n == 0 {
+            dp[k][0] = 1
+        } else {
+            dp[k][n] = (dp[k-1][n]+dp[k][n-1]) % 1_000_000_000
+        }
+    }
+}
+
+print(dp[nk[1]][nk[0]])
